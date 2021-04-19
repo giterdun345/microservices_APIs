d95542a8f5725c71e296585242e4a0fb8458b70a
John Ketterer <johnmketterer@gmail.com>
2021-04-18 21:03:48 -0500

    css added

--- myApp.js
+++ myApp.js
@@ -6,8 +6,9 @@ app.get('/', (req, res)=>{
   res.sendFile(__dirname + '/views/index.html')
 })
 
-app.use(express.static(__dirname + '/public/style.css'))
 
+// app.use(express.static(__dirname + '/public/style.css'))
+app.use("/public", express.static(__dirname + "/public"))
 
 
 
