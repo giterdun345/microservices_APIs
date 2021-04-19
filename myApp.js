8507abaa4b4ed8a8aea298f53be9c211f8f7fb9d
John Ketterer <johnmketterer@gmail.com>
2021-04-18 21:08:15 -0500

    json response added and pointed to

--- myApp.js
+++ myApp.js
@@ -6,10 +6,12 @@ app.get('/', (req, res)=>{
   res.sendFile(__dirname + '/views/index.html')
 })
 
-
 // app.use(express.static(__dirname + '/public/style.css'))
 app.use("/public", express.static(__dirname + "/public"))
 
+app.get('/json', (req, res)=>{
+  res.send({"message": "Hello json"})
+})
 
 
 
